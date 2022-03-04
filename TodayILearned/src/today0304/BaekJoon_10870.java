0579495ed28359ee35dcd4af546a76170ecc38aa
Ahn Jungeun <iszrlo17@gmail.com>
2022-03-04 22:32:32 +0900

    백준 문제 풀이

---
+++ TodayILearned/src/today0304/BaekJoon_10870.java
@@ -0,0 +1,24 @@
+package today0304;
+/*
+	피보나치 수 : 0과 1로 시작
+	Fn = Fn-1 + Fn-2 (n ≥ 2)
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BaekJoon_10870 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		System.out.println(function(n));
+	}//main()
+	
+	public static int function(int n) {
+		
+		if(n==0 | n==1) return n;
+		
+		return function(n-2) + function(n-1);
+		
+	}//function()
+}//class
